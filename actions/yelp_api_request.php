33348a33d9aa5b1921bfcda935e69c03461b2ad4
davisliu300 <davis.liu300@gmail.com>
2015-04-14 11:38:43 -0700

    consolidate directory

--- actions/yelp_api_request.php
+++ actions/yelp_api_request.php
@@ -8,9 +8,9 @@ session_start();
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
   
-  <script src = "../Assets/js_yelp.js"></script>
+  <script src = "../assets/js_yelp.js"></script>
   
-  <link rel ="stylesheet" href = "../Assets/style_yelp.css">
+  <link rel ="stylesheet" href = "../assets/style_yelp.css">
   <style> 
   </style> 
   </head> 
